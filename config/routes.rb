7c814e7d30cca7536568b9e3bf6aba906bddb785
joskalenda <joskalenda3@gmail.com>
2022-04-27 18:48:00 +0200

    Create posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  resources :users do
+    resources :comments
+  end
   root "home#index"
 end
