20db9f8a0d86665cc34b822be7516728e73326f0
joskalenda <joskalenda3@gmail.com>
2022-04-28 16:44:07 +0200

    fix linters

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,20 +1,18 @@
-require "rails_helper"
+require 'rails_helper'
 
-RSpec.describe "Posts controller", :type => :request do
-
+RSpec.describe 'Posts controller', type: :request do
   before(:each) { get user_posts_path user_id: 0 }
   it 'should return correct response' do
     expect(response).to have_http_status(200)
   end
 
-  it "This should render one single user posts page" do
+  it 'This should render one single user posts page' do
     expect(response).to render_template(:index)
-    expect(response.body).to include("THIS SINGLE USER POSTS")
+    expect(response.body).to include('THIS SINGLE USER POSTS')
   end
-  it "This should render one single user single post page" do
-    get "/users/0/posts/0"
+  it 'This should render one single user single post page' do
+    get '/users/0/posts/0'
     expect(response).to render_template(:show)
-    expect(response.body).to include("THIS SINGLE USER SELECTED POST")
+    expect(response.body).to include('THIS SINGLE USER SELECTED POST')
   end
-
 end
