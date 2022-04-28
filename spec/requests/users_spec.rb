5489a02012c103e3e4af831a2f246cbd6dc419cc
joskalenda <joskalenda3@gmail.com>
2022-04-28 15:14:12 +0200

    Add test for user

---
+++ spec/requests/users_spec.rb
@@ -0,0 +1,21 @@
+require "rails_helper"
+
+RSpec.describe "Users controller", :type => :request do
+
+  it 'should return 200 response' do
+    get "/users"
+    expect(response).to have_http_status(200)
+  end
+
+  it "This should redirect to all users page" do
+    get "/users"
+    expect(response).to render_template(:index)
+    expect(response.body).to include("ALL USERS")
+  end
+
+  it "does not render a different template" do
+    get "/users"
+    expect(response).to_not render_template(:show)
+  end
+
+end
