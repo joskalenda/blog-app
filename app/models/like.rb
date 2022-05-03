1a865cabe3238db3c87b1d9c001979a54cc3cd3d
joskalenda <joskalenda3@gmail.com>
2022-05-03 23:40:48 +0200

    Update methods for user

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 class Like < ApplicationRecord
+  after_save :increment_likes_counter
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
-  after_save :increment_likes_counter
   
   def increment_likes_counter
     post.increment!(:likes_counter)
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
 class Post < ApplicationRecord
-  belongs_to :user
+  after_save :increment_posts_counter
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  has_many :comments
+  has_many :likes
+
+  def latest_five_comment
+    comments.order(created_at: :desc).limit(5)
+  end
+
+  def increment_posts_counter
+    user.increment!(:posts_counter)
+  end
+  
+  private :increment_posts_counter
 end
