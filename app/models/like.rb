3956c586c0764bd585aa26cdacbc7da6dbc631dd
joskalenda <joskalenda3@gmail.com>
2022-05-03 22:53:54 +0200

    Create methode to increment likes_counter

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
+  after_save :increment_likes_counter
+  
+  def increment_likes_counter
+    post.increment!(:likes_counter)
+  end
+  
+  private :increment_likes_counter
+
 end
