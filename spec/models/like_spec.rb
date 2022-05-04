0d4539f0b797439ac1a3b2cb4fe98a36e98c19fe
joskalenda <joskalenda3@gmail.com>
2022-05-04 10:06:17 +0200

    Add testing for like

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Should add some likes to posts and comments' do
+    like = Like.new
+    like.build_user(name: 'Kalenda')
+    like.build_post(text: 'Happy')
+    expect(like).to be_valid
+  end
+
+  it 'Should increment likes_counter' do
+    user = User.new(name: 'Topaz')
+    user2 = User.create!(name: 'Mercure')
+    post = user2.posts.create!(title: 'Enjoy coding book', text: 'Learn how to code')
+    post.likes.create!(user: user)
+    expect(post.likes_counter).to eql(1)
+  end
 end
