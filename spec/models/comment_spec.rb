881023bcb3e7d0fc146701092421a739768421ce
joskalenda <joskalenda3@gmail.com>
2022-05-04 10:05:47 +0200

    Add testing for comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'Should create comment' do
+    user = User.new(name: 'Jos', photo: '', bio: 'Developer', posts_counter: 0)
+    post = Post.new(title: 'Coding war', text: 'Learn how to code', user: user)
+    post.save!
+
+    comment_creator = User.new(name: 'Ben', photo: '', bio: 'Fan', posts_counter: 0)
+    post.comments.create!(text: 'i love coding, thanks Jos', user: comment_creator)
+    post.comments.create!(text: 'Would you like to share some?', user: comment_creator)
+    expect(post.comments.length).to eql(2)
+  end
 end
