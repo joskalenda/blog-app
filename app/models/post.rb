36bf8ceade831cb52f38aa1eb3b9e933cb9849a4
joskalenda <joskalenda3@gmail.com>
2022-05-06 23:26:42 +0200

    Update validation for post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   validates_presence_of :title, length: { maximum: 250 }
-  # validates :comments_counter, :likes_counter, presence: true,
-  #   numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :comments_counter, :likes_counter, presence: true,
+    numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 
   after_save :increment_posts_counter
   belongs_to :user, class_name: 'User', foreign_key: 'user_id'
