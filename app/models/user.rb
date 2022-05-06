e37482cbce73a4cc33798dbc4377dc09fa0d4b4c
joskalenda <joskalenda3@gmail.com>
2022-05-06 21:40:24 +0200

    Add validation for coounter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
   validates_presence_of :name
+  validates :posts_counter, presence: true,
+    numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+
   has_many :posts, foreign_key: 'user_id', dependent: :destroy
   has_many :comments, foreign_key: 'user_id', dependent: :destroy
   has_many :likes, foreign_key: 'user_id', dependent: :destroy
