d3dd110a0498a45ce26dad9041afd0fb1811f6e7
joskalenda <joskalenda3@gmail.com>
2022-05-12 14:55:08 +0200

    Install devide gem file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   get 'comments/new'
   get 'likes/index'
   get 'likes/show'
@@ -10,5 +11,6 @@ Rails.application.routes.draw do
       resources :likes, only: [:new, :create]
     end
   end
-  root 'home#index'
+  # root 'home#index'
+  root to: "home#index"
 end
